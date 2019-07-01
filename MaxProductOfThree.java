a68c1fef7d96826357c4de1a0375f405247470c1
Miroslav Mogus <miromogus@gmail.com>
2019-07-01 15:03:12 +0200

    Readme added

--- MaxProductOfThree.java
+++ MaxProductOfThree.java
@@ -6,12 +6,23 @@ class MaxProductOfThree {
         
         int maxProduct;
 
+        /*
+        * Not a triplet.
+        */
+
         if (n < 3) {
             return -1;
         }
 
         Arrays.sort(arr);
 
+        /*
+        * Most important part.
+        * Negative and negative number give a positive one.
+        * This is the reason for the first check.
+        * Example: -20 * -8 = 160 * 15 = 2400
+        */
+        
         maxProduct = Math.max(arr[0]     * arr[1]     * arr[n - 1],
                               arr[n - 1] * arr[n - 2] * arr[n - 3]
                              );
